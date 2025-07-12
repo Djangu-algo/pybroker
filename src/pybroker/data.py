3c40a5e348bab3f7f9ec12023bf5f5ee012dcfe3
Denys Smerchanskyi <78853680+Djangu-algo@users.noreply.github.com>
2025-07-12 23:42:05 +1000

    Update data.py
    Edited data module

--- src/pybroker/data.py
+++ src/pybroker/data.py
@@ -6,19 +6,12 @@ This code is licensed under Apache 2.0 with Commons Clause license
 (see LICENSE for details).
 """
 
-import itertools
 from abc import ABC, abstractmethod
 from datetime import datetime
-from typing import Any, Final, Iterable, Optional, Union
+from typing import Any, Iterable, Optional, Union
 
-import alpaca.data.historical.crypto as alpaca_crypto
-import alpaca.data.historical.stock as alpaca_stock
 import numpy as np
 import pandas as pd
-import yfinance
-from alpaca.data.enums import Adjustment
-from alpaca.data.requests import CryptoBarsRequest, StockBarsRequest
-from alpaca.data.timeframe import TimeFrame, TimeFrameUnit
 
 from pybroker.cache import DataSourceCacheKey
 from pybroker.common import (
@@ -221,8 +214,8 @@ class DataSource(ABC, DataSourceCacheMixin):
         )
         if not uncached_syms:
             return cached_df
-        self._logger.download_bar_data_start()
-        self._logger.info_download_bar_data_start(
+        self._logger.fetch_bar_data_start()
+        self._logger.info_fetch_bar_data_start(
             symbols=uncached_syms,
             timeframe=timeframe,
             start_date=start_date,
@@ -244,7 +237,7 @@ class DataSource(ABC, DataSourceCacheMixin):
         df = pd.concat((cached_df, df))
         if not df.empty:
             df = df.sort_values(by=[DataCol.DATE.value, DataCol.SYMBOL.value])
-        self._logger.download_bar_data_completed()
+        self._logger.fetch_bar_data_completed()
         return df.reset_index(drop=True)
 
     @abstractmethod
@@ -289,282 +282,3 @@ class DataSource(ABC, DataSourceCacheMixin):
         return " ".join(
             f"{part[0]}{part[1]}" for part in parse_timeframe(timeframe)
         )
-
-
-def _parse_alpaca_timeframe(
-    timeframe: Optional[str],
-) -> tuple[int, TimeFrameUnit]:
-    if timeframe is None:
-        raise ValueError("Timeframe needs to be specified for Alpaca.")
-    parts = parse_timeframe(timeframe)
-    if len(parts) != 1:
-        raise ValueError(f"Invalid Alpaca timeframe: {timeframe}")
-    tf = parts[0]
-    if tf[1] == "min":
-        unit = TimeFrameUnit.Minute
-    elif tf[1] == "hour":
-        unit = TimeFrameUnit.Hour
-    elif tf[1] == "day":
-        unit = TimeFrameUnit.Day
-    elif tf[1] == "week":
-        unit = TimeFrameUnit.Week
-    else:
-        raise ValueError(f"Invalid Alpaca timeframe: {timeframe}")
-    return tf[0], unit
-
-
-class Alpaca(DataSource):
-    """Retrieves stock data from `Alpaca <https://alpaca.markets/>`_."""
-
-    __EST: Final = "US/Eastern"
-
-    def __init__(self, api_key: str, api_secret: str):
-        super().__init__()
-        self._api = alpaca_stock.StockHistoricalDataClient(api_key, api_secret)
-
-    def query(
-        self,
-        symbols: Union[str, Iterable[str]],
-        start_date: Union[str, datetime],
-        end_date: Union[str, datetime],
-        timeframe: Optional[str] = "1d",
-        adjust: Optional[Any] = None,
-    ) -> pd.DataFrame:
-        _parse_alpaca_timeframe(timeframe)
-        return super().query(symbols, start_date, end_date, timeframe, adjust)
-
-    def _fetch_data(
-        self,
-        symbols: frozenset[str],
-        start_date: datetime,
-        end_date: datetime,
-        timeframe: Optional[str],
-        adjust: Optional[Any],
-    ) -> pd.DataFrame:
-        """:meta private:"""
-        amount, unit = _parse_alpaca_timeframe(timeframe)
-        adj_enum = None
-        if adjust is not None:
-            for member in Adjustment:
-                if member.value == adjust:
-                    adj_enum = member
-                    break
-            if adj_enum is None:
-                raise ValueError(f"Unknown adjustment: {adjust}.")
-        request = StockBarsRequest(
-            symbol_or_symbols=list(symbols),
-            start=start_date,
-            end=end_date,
-            timeframe=TimeFrame(amount, unit),
-            limit=None,
-            adjustment=adj_enum,
-            feed=None,
-        )
-        df = self._api.get_stock_bars(request).df  # type: ignore[union-attr]
-        if df.columns.empty:
-            return pd.DataFrame(
-                columns=[
-                    DataCol.SYMBOL.value,
-                    DataCol.DATE.value,
-                    DataCol.OPEN.value,
-                    DataCol.HIGH.value,
-                    DataCol.LOW.value,
-                    DataCol.CLOSE.value,
-                    DataCol.VOLUME.value,
-                    DataCol.VWAP.value,
-                ]
-            )
-        if df.empty:
-            return df
-        df = df.reset_index()
-        df.rename(columns={"timestamp": DataCol.DATE.value}, inplace=True)
-        df = df[[col.value for col in DataCol]]
-        df[DataCol.DATE.value] = pd.to_datetime(df[DataCol.DATE.value])
-        df[DataCol.DATE.value] = df[DataCol.DATE.value].dt.tz_convert(
-            self.__EST
-        )
-        return df
-
-
-class AlpacaCrypto(DataSource):
-    """Retrieves crypto data from `Alpaca <https://alpaca.markets/>`_.
-
-    Args:
-        api_key: Alpaca API key.
-        api_secret: Alpaca API secret.
-    """
-
-    TRADE_COUNT: Final = "trade_count"
-    COLUMNS: Final = (
-        DataCol.SYMBOL.value,
-        DataCol.DATE.value,
-        DataCol.OPEN.value,
-        DataCol.HIGH.value,
-        DataCol.LOW.value,
-        DataCol.CLOSE.value,
-        DataCol.VOLUME.value,
-        DataCol.VWAP.value,
-        TRADE_COUNT,
-    )
-
-    __EST: Final = "US/Eastern"
-
-    def __init__(self, api_key: str, api_secret: str):
-        super().__init__()
-        self._scope.register_custom_cols(self.TRADE_COUNT)
-        self._api = alpaca_crypto.CryptoHistoricalDataClient(
-            api_key, api_secret
-        )
-
-    def query(
-        self,
-        symbols: Union[str, Iterable[str]],
-        start_date: Union[str, datetime],
-        end_date: Union[str, datetime],
-        timeframe: Optional[str] = "1d",
-        _adjust: Optional[str] = None,
-    ) -> pd.DataFrame:
-        _parse_alpaca_timeframe(timeframe)
-        return super().query(symbols, start_date, end_date, timeframe, _adjust)
-
-    def _fetch_data(
-        self,
-        symbols: frozenset[str],
-        start_date: datetime,
-        end_date: datetime,
-        timeframe: Optional[str],
-        _adjust: Optional[str],
-    ) -> pd.DataFrame:
-        """:meta private:"""
-        amount, unit = _parse_alpaca_timeframe(timeframe)
-        request = CryptoBarsRequest(
-            symbol_or_symbols=list(symbols),
-            start=start_date,
-            end=end_date,
-            timeframe=TimeFrame(amount, unit),
-            limit=None,
-        )
-        df = self._api.get_crypto_bars(request).df  # type: ignore[union-attr]
-        if df.columns.empty:
-            return pd.DataFrame(columns=self.COLUMNS)
-        if df.empty:
-            return df
-        df = df.reset_index()
-        df.rename(columns={"timestamp": DataCol.DATE.value}, inplace=True)
-        df = df[[col for col in self.COLUMNS]]
-        df[DataCol.DATE.value] = pd.to_datetime(df[DataCol.DATE.value])
-        df[DataCol.DATE.value] = df[DataCol.DATE.value].dt.tz_convert(
-            self.__EST
-        )
-        return df
-
-
-class YFinance(DataSource):
-    r"""Retrieves data from `Yahoo Finance <https://finance.yahoo.com/>`_\ .
-
-    Args:
-        auto_adjust: Whether to auto adjust close prices. If ``True``, then
-            adjusted close prices are stored in the ``close`` column. Defaults
-            to ``False``.
-
-    Attributes:
-        ADJ_CLOSE: Column name of adjusted close prices.
-    """
-
-    ADJ_CLOSE: Final = "adj_close"
-    __TIMEFRAME: Final = "1d"
-
-    def __init__(self, auto_adjust: bool = False):
-        super().__init__()
-        self.auto_adjust = auto_adjust
-        self._scope.register_custom_cols(self.ADJ_CLOSE)
-
-    def query(
-        self,
-        symbols: Union[str, Iterable[str]],
-        start_date: Union[str, datetime],
-        end_date: Union[str, datetime],
-        _timeframe: Optional[str] = "",
-        _adjust: Optional[Any] = None,
-    ) -> pd.DataFrame:
-        r"""Queries data from `Yahoo Finance <https://finance.yahoo.com/>`_\ .
-        The timeframe of the data is limited to per day only.
-
-        Args:
-            symbols: Ticker symbols of the data to query.
-            start_date: Start date of the data to query (inclusive).
-            end_date: End date of the data to query (inclusive).
-
-        Returns:
-            :class:`pandas.DataFrame` containing the queried data.
-        """
-        return super().query(
-            symbols, start_date, end_date, self.__TIMEFRAME, _adjust
-        )
-
-    def _fetch_data(
-        self,
-        symbols: frozenset[str],
-        start_date: datetime,
-        end_date: datetime,
-        _timeframe: Optional[str],
-        _adjust: Optional[Any],
-    ) -> pd.DataFrame:
-        """:meta private:"""
-        show_yf_progress_bar = (
-            not self._logger._disabled
-            and not self._logger._progress_bar_disabled
-        )
-        df = yfinance.download(
-            list(symbols),
-            start=start_date,
-            end=end_date,
-            progress=show_yf_progress_bar,
-            auto_adjust=self.auto_adjust,
-        )
-        if df.columns.empty:
-            columns = [
-                DataCol.SYMBOL.value,
-                DataCol.DATE.value,
-                DataCol.OPEN.value,
-                DataCol.HIGH.value,
-                DataCol.LOW.value,
-                DataCol.CLOSE.value,
-                DataCol.VOLUME.value,
-            ]
-            if not self.auto_adjust:
-                columns.append(self.ADJ_CLOSE)
-            return pd.DataFrame(columns=columns)
-        if df.empty:
-            return df
-        df = df.reset_index()
-        result = pd.DataFrame()
-        if len(symbols) == 1:
-            result[DataCol.DATE.value] = df["Date"].values
-            result[DataCol.SYMBOL.value] = tuple(
-                itertools.repeat(next(iter(symbols)), len(df["Close"].values))
-            )
-            result[DataCol.OPEN.value] = df["Open"].values
-            result[DataCol.HIGH.value] = df["High"].values
-            result[DataCol.LOW.value] = df["Low"].values
-            result[DataCol.CLOSE.value] = df["Close"].values
-            result[DataCol.VOLUME.value] = df["Volume"].values
-            if not self.auto_adjust:
-                result[self.ADJ_CLOSE] = df["Adj Close"].values
-        else:
-            df.columns = df.columns.to_flat_index()
-            for sym in symbols:
-                sym_df = pd.DataFrame()
-                sym_df[DataCol.DATE.value] = df[("Date", "")].values
-                sym_df[DataCol.SYMBOL.value] = tuple(
-                    itertools.repeat(sym, len(df[("Close", sym)].values))
-                )
-                sym_df[DataCol.OPEN.value] = df[("Open", sym)].values
-                sym_df[DataCol.HIGH.value] = df[("High", sym)].values
-                sym_df[DataCol.LOW.value] = df[("Low", sym)].values
-                sym_df[DataCol.CLOSE.value] = df[("Close", sym)].values
-                sym_df[DataCol.VOLUME.value] = df[("Volume", sym)].values
-                if not self.auto_adjust:
-                    sym_df[self.ADJ_CLOSE] = df[("Adj Close", sym)].values
-                result = pd.concat((result, sym_df))
-        return result
